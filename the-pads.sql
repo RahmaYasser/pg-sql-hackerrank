544e4bff876a849e2d73888296a1540b6b92a2ef
RahmaYasser <rahma.y9079@gmail.com>
2022-07-30 15:23:40 +0200

    the-pads

--- the-pads.sql
+++ the-pads.sql
@@ -5,6 +5,14 @@ SELECT concat(name,'(',LEFT(occupation,1),')')
 FROM occupations
 
 --second set
-SELECT concat('There are a total ', COUNT(*), ' of ', LOWER(occupation),'s')
+
+SELECT concat('There are a total ', COUNT(*) , ' of ', LOWER(occupation),'s') 
 FROM occupations
 GROUP BY occupation
+ORDER BY COUNT(*)
+
+
+
+
+-- useful links
+-- https://stackoverflow.com/questions/19601948/must-appear-in-the-group-by-clause-or-be-used-in-an-aggregate-function
