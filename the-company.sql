f15b11ab2ed81836104b346a7d5549766c53132c
RahmaYasser <rahma.y9079@gmail.com>
2022-07-30 17:28:35 +0200

    the-company

---
+++ the-company.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/the-company/problem
+
+
+SELECT e.company_code, c.founder, COUNT(DISTINCT e.lead_manager_code), COUNT(DISTINCT e.senior_manager_code), COUNT(DISTINCT e.manager_code), COUNT(DISTINCT e.employee_code)
+FROM Employee as e
+INNER JOIN Company As c
+ON c.company_code = e.company_code
+GROUP BY c.company_code,c.founder
+ORDER BY c.company_code;
